fb12b5c897a8350cb42cf2561b24287615abcdc3
Macropays <royalajibade@gmail.com>
2026-07-18 10:29:17 +0100

    Add Tailwind CSS configuration with custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { 
+    extend: {
+      colors: {
+        'macro-blue': '#0a0f1e',
+        'macro-gold': '#fbbf24'
+      }
+    }
+  },
+  plugins: [],
+}
